9a58d3cb2348566971e1f3affee51cf14cceeb75
davut <davutozcan87@gmail.com>
2020-01-26 18:09:15 +0300

    controller path fix

--- spring/src/main/java/com/devoxx/spring/demo/test/HelloController.java
+++ spring/src/main/java/com/devoxx/spring/demo/test/HelloController.java
@@ -1,10 +1,13 @@
+package com.devoxx.spring.demo.test;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController{
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(){
-        return "Hello from spring";
+        return "Hello spring";
     }
 }
