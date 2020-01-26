8b15cda48b8e05ebb09c5a127431195d5b68ec14
davut <davutozcan87@gmail.com>
2020-01-26 13:47:23 +0300

    quarqus created from maven archetype

---
+++ create.sh
@@ -0,0 +1,5 @@
+mvn io.quarkus:quarkus-maven-plugin:1.1.1.Final:create \
+    -DprojectGroupId=com.devoxx.demo \
+    -DprojectArtifactId=first-quarqus \
+    -DprojectVersion=0.0.1 \
+    -DclassName="com.devox.demo.MyResource"
